0fb034df57cd00012453db67034192c9143f097e
Victor Manuel Aponte Paredes <91540714+VictorAponteParedes@users.noreply.github.com>
2021-11-02 16:30:50 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema
 
 
 const UserSchema = new Schema({
-
-    nationality:{ type:String,require:true  },
+     
+    user:{nationality:{ type:String,require:true  },
 
     name:{type:String, require:true  },
 
@@ -13,6 +13,24 @@ const UserSchema = new Schema({
     dateOfBirth:{ type:Date,  require:true  },
 
     nameRoute:{ type:String, require:true  },
+
+    Address:{
+        number:{ type:Number, require:true },
+
+        flat:{ type:Number   },
+    
+        zipCode:{ type:Number,  require:true  },
+    
+        contactNumber:{type:Number,require:true },
+        },
+         },
+    
+    
+    email:{ type:String, require:true  },
+
+    password:{ type:String, require:true  },
+
+    createDate:{ type:Date,require:true  },  
 
 
 
